98673d69cba80c608b7225e9e0c7fb0b4a59043f
ikmalil birri <malikmal98@gmail.com>
2020-12-30 03:18:05 +0700

    other way

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
     /* config options here */
     // options: {
     //     dist: '.next'
     //   }
-      distDir: 'out',
+    distDir: 'out',
+    assetPrefix: isProd ? 'https://cdn.statically.io/gh/Malikmal/malikmal.github.io/gh-pages/' : '',
   }
