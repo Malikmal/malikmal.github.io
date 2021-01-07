9318c77a3e97e1a75f5f23d1831134e5ee4cc413
ikmalil birri <malikmal98@gmail.com>
2021-01-07 07:39:39 +0700

    test again

--- pages/dispatch.js
+++ pages/dispatch.js
@@ -6,35 +6,35 @@ import userSWR from 'swr'
 // const DISPATCH_GITHUB_TOKEN = process.env.DISPATCH_GITHUB_TOKEN;
 // const fetcher = (...args) => fetch(...args).then(res => res.json())
 const url = "https://api.github.com/repos/malikmal/malikmal.github.io/actions/workflows/dispatch.yml/dispatches";
-const fetcher = (url) => fetch(url, {
-    method: 'POST',
-    headers: {
-         'Content-Type': 'application/json',
-         'Authorization' : `token fd895ef3226d30f8ff18f7e27b2d839da246283a`,
-         'Accept' : "application/vnd.github.v3+json",
-    },
-    body: JSON.stringify({ 'ref': 'master' })
-}).then(res => res.json())
+// const fetcher = (url) => fetch(url, {
+//     method: 'POST',
+//     headers: {
+//          'Content-Type': 'application/json',
+//          'Authorization' : `token fd895ef3226d30f8ff18f7e27b2d839da246283a`,
+//          'Accept' : "application/vnd.github.v3+json",
+//     },
+//     body: JSON.stringify({ 'ref': 'master' })
+// }).then(res => res.json())
 
-// const fetcher = url => axios.post(url, {"ref":"master"}, {
-//     headers : {
-//         Authorization : `token ${process.env.DISPATCH_GITHUB_TOKEN}`,
-//         Accept : "application/vnd.github.v3+json",
-//         // "Content-Type" : "application/json",
-//         // "Content-Length" : "1024",
-//         // Host : "malikmal98.github.io",
-//         // "User-Agent" : "Malikmal",
-//         // "Accept-Encoding" : "gzip, deflate, br",
-//         // Connection : "keep-alive",
-//         // "Access-Control-Allow-Origin": "*",
-//         // "Access-Control-Allow-Credentials" : "true",
-//         // "Access-Control-Allow-Methods" : "GET,HEAD,OPTIONS,POST,PUT",
-//         // "Access-Control-Allow-Headers" : "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
-//     }
-// }).then(res => res.data)
-// .catch(err => {
-//     console.log(err);
-// })
+const fetcher = url => axios.post(url, {"ref":"master"}, {
+    headers : {
+        Authorization : `token ${process.env.DISPATCH_GITHUB_TOKEN}`,
+        Accept : "application/vnd.github.v3+json",
+        "Content-Type" : "application/json",
+        // "Content-Length" : "1024",
+        // Host : "malikmal98.github.io",
+        // "User-Agent" : "Malikmal",
+        // "Accept-Encoding" : "gzip, deflate, br",
+        // Connection : "keep-alive",
+        // "Access-Control-Allow-Origin": "*",
+        // "Access-Control-Allow-Credentials" : "true",
+        // "Access-Control-Allow-Methods" : "GET,HEAD,OPTIONS,POST,PUT",
+        // "Access-Control-Allow-Headers" : "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
+    }
+}).then(res => res.data)
+.catch(err => {
+    console.log(err);
+})
 
 
 export default function dispatch(){
@@ -45,7 +45,32 @@ export default function dispatch(){
     if (!data) return <div>loading...</div>
     // render data
     return <div>hello !</div>
-    //////
+
+    // Axios.post(url, {"ref":"master"},{
+    //     headers: {
+    //         Authorization: `token ${process.env.DISPATCH_GITHUB_TOKEN}`,
+    //         Accept : "application/vnd.github.v3+json",
+    //         "Content-Type" : "application/json",
+    //     }
+    // }).then(res => {
+    //     console.log("ok");
+    // })
+
+    // axios.post(url, {"ref":"master"},{
+    //     headers: {
+    //         Authorization: `token fd895ef3226d30f8ff18f7e27b2d839da246283a`,
+    //         Accept : "application/vnd.github.v3+json"
+    //     }
+    // })
+    // .then(res => {
+    //     console.log(res)
+    // })
+    // .catch(err => {
+    //     console.error(err); 
+    // })
+
+    // return <h2>ok</h2>;
+        //////
 
     // console.log(process.env.WORDPRESS_API_URL);
     // if (!process.env.WORDPRESS_API_URL) {
@@ -59,7 +84,7 @@ export default function dispatch(){
     //         Accept : "application/vnd.github.v3+json"
     //     }
     // })   
-    // // return res.status;
+    // return res.status;
     // return <h1>successs</h1>
 
     // const headers = { 
